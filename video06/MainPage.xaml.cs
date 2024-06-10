e761af8b63100cf5683648beb2a9d42162abbb93
MINEDUCYT <MINEDUCYT@LAPTOP-7SKAH6B6>
2024-06-10 05:53:25 -0600

    Agregar archivos de proyecto.

---
+++ video06/MainPage.xaml.cs
@@ -0,0 +1,24 @@
+﻿using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Logging.Debug;
+
+namespace video06
+{
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private void Entry_TextChanged(object sender, EventArgs e)
+        {
+           string pasado= e.OldTextValue;
+            string nuevo= e.NewTextValue;
+        }
+        private void Entry_Completed(object sender, EventArgs e)
+        {
+            string text = ((Entry)sender).Text;
+        }
+    }
+
+}
